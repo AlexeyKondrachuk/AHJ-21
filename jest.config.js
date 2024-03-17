3658b99419c9bc30ee9a2e1b4abfdcbf76eeef03
Alexey.K <lexxtlt@gmail.com>
2024-03-17 14:36:35 +0400

    jest

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    moduleFileExtensions: ['ts', 'tsx', 'js'],
-    globals: { 'ts-jest': { tsConfig: 'tsconfig.json' } },
-    testMatch: ['**/test/**/*.test.(ts|tsx)'],
-    // ...
-    passWithNoTests: true,
-  };
