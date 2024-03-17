105b26e94ca404dcda2dad412b29fbc8e76af114
Alexey.K <lexxtlt@gmail.com>
2024-03-17 15:08:29 +0400

    webpack

--- webpack.common.js
+++ webpack.common.js
@@ -3,14 +3,12 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  passWithNoTests: true,
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '',
   },
   module: {
-    passWithNoTests: true,
     rules: [
       {
         test: /\.js$/,
